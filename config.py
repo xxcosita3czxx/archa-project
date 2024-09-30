e11318ddea87103ac117748deba6f350b1b961ec
huewhg <adam.dragon.karasek@gmail.com>
2024-09-30 18:12:16 +0200

    update 1 file and move 1 file

--- config.py
+++ config.py
@@ -6,19 +6,3 @@ class Config:
     TEMPLATES_AUTO_RELOAD = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'project.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# database.py
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Location(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    row = db.Column(db.Integer, nullable=False)
-    col = db.Column(db.Integer, nullable=False)
-
-class Setup(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    cols = db.Column(db.Integer, default=5)
-    rows = db.Column(db.Integer, default=5)
