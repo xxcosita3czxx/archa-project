76885823299c77eab42b1519d09570785d6e3827
xxcosita3czxx <kokosove18@gmail.com>
2024-09-30 17:56:26 +0200

    moving api

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, send_from_directory
-from flask_restx import Api, Resource
+from api import api
 
 app = Flask(__name__, static_folder='frontend')
-api = Api(app, doc='/swagger')  # Swagger UI at /swagger
+
 
 # Serve frontend files from 'frontend/' directory as root
 @app.route('/')
@@ -10,14 +10,7 @@ api = Api(app, doc='/swagger')  # Swagger UI at /swagger
 def serve_frontend(path='index.html'):
     return send_from_directory(app.static_folder, path)
 
-# Create a namespace for the API
-ns = api.namespace('api', description='API operations')
-
-# API Endpoint
-@ns.route('/hello')
-class HelloWorld(Resource):
-    def get(self):
-        return {'message': 'Hello, World!'}
+api(app=app)
 
 if __name__ == '__main__':
     app.run(debug=True)
