ed35291a656a0c655958d90544a348edc7efbd51
huewhg <adam.dragon.karasek@gmail.com>
2024-09-30 18:08:42 +0200

    gersiubugiuse

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from api import api
 import sqlalchemy
-from database import 
+from database import test
 
 app = Flask(__name__, static_folder='./frontend', template_folder='./frontend')
 
