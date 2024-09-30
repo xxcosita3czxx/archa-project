dce8058d65b46576973c2a7f933c0f8a03fd81ba
xxcosita3czxx <kokosove18@gmail.com>
2024-09-30 19:14:05 +0200

    readable db edit

--- database.py
+++ database.py
@@ -2,9 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
-def querydb(table):
-    q = table.query.all()
-    return q
+
+class dbEdit:
+    def dumpall(table):
+        q = table.query.all()
+        return q
+
 class test(db.Model):
     id: Mapped[int] = mapped_column(db.Integer, primary_key=True, autoincrement=True)
     huewhg: Mapped[str] = mapped_column(db.String, nullable=False)
