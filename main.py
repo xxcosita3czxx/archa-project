9d2e45f6b4386dbbbac7ba029cd52d3b5308349b
huewhg <adam.dragon.karasek@gmail.com>
2024-09-30 18:00:34 +0200

    update api.cpython-312.pyc and main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, send_from_directory, render_template
 from api import api
 
-app = Flask(__name__, static_folder='frontend')
+app = Flask(__name__, static_folder='./frontend')
 
 
 # Serve frontend files from 'frontend/' directory as root
@@ -10,13 +10,9 @@ def hello_world():
     return render_template("index.html")
 
 # Create a namespace for the API
-ns = api.namespace('api', description='API operations')
+api(app=app)
 
 # API Endpoint
-@ns.route('/hello')
-class HelloWorld(Resource):
-    def get(self):
-        return {'message': 'Hello, World!'}
 
 if __name__ == '__main__':
     app.run(debug=True)
